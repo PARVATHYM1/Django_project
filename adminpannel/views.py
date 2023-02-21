61cf014b8fd1376fae3c38e0b87498250602e3bb
PARVATHYM123 <parvathymini2000@gmail.com>
2023-02-21 20:33:45 +0530

    form and veiws

--- adminpannel/views.py
+++ adminpannel/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+def loginadmin(request):
+    if request.user.is_authenticated:
+        return HttpResponse('You Are already logged in')
+    else:	
+        if request.method == 'POST':
+            login_form = LoginForm(request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data['username']
+                password = login_form.cleaned_data['password']
+
+                user = authenticate(username=username, password=password)
+                                
+                if user is not None:
+                    if user.is_active and user.is_superuser:
+                        login(request,user)	
+                        return HttpResponse('Login Successful')
+                    else:
+                        return HttpResponse('Your account is not active')
+                else:
+                    return HttpResponse('The Account does not exists')
+            else:
+                login_form = LoginForm()
+                return render(request, "adminpannel/login.html",{"form":login_form})
+        else:
+            login_form = LoginForm()
+        return render(request,'adminpannel/login.html',{"form":login_form})
